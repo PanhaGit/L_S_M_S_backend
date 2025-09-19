ff24c05a14fc07d65bc609c9de17d8a333226ef7
Panha <thopanha123@gmail.com>
2025-09-19 15:27:08 +0700

    setup project

---
+++ src/main/java/com/team_yerng/l_s_m_s/LSMSApplication.java
@@ -0,0 +1,13 @@
+package com.team_yerng.l_s_m_s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LSMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LSMSApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team_yerng/l_s_m_s/LSMSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_yerng.l_s_m_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LSMSApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
